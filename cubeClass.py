f2bcfa16d2ed5333a8e222e2784a7973f203ef84
ryan.foley <rfoley@hawaii.edu>
2022-06-16 10:02:57 -1000

    Work from 6/15 Started work on class and modules under core

--- cubeClass.py
+++ cubeClass.py
@@ -4,47 +4,39 @@ from pycube.core import background
 from pycube import msgs
 from astropy.io import fits
 
-class Cube:
-    def __init__(self, image,primary = None,data = None,stats = None, background_mode = None):
+
+class IFU_Cube:
+    def __init__(self, image, primary=None, data=None, stat=None, background_mode=None):
+        """
+        initalizes data cube FITS file
+        Args:
+            image:
+            primary:
+            data:
+            stat:
+            background_mode:
+        """
         self.image = image
         self.primary = primary
         self.data = data
-        self.stats = stats
+        self.stat = stat
 
     def from_fits_file(self, fits_filename):
         hdul = fits.open(fits_filename)
         self.primary = hdul[0]
         self.data = hdul[1]
-        self.stats = hdul[2]
-        
-"""
-    def extract_vals(self):
-        bgMedian = np.nanmedian(data_val,0)
-        bgSigma = np.sqrt(np.nanmedian(stat_val,0))
-        bgMask = np.zeros_like(data_val,0)
-"""
+        self.stat = hdul[2]
 
+    def extract_vals(self):
+        bgMedian = np.nanmedian(self.data, 0)
+        bgSigma = np.sqrt(np.nanmedian(self.stat, 0))
+        bgMask = np.zeros_like(self.data, 0)
 
     def background(self, mode='median'):
         if mode == 'median':
             backgrd = background.median_background(self.data)
         elif mode == 'sextractor':
-            # ToDo implement this
             backgrd = background.sextractor_background(self.data)
         else:
             raise ValueError
             msgs.warning('Possible values are:\n {}'.format(background.BACKGROUND_MODES))
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
--- pycube/core/background.py
+++ pycube/core/background.py
@@ -1,9 +1,11 @@
 import numpy as np
+from scipy import stats
+
 BACKGROUND_MODES = ['median', 'sextractor']
+
 
 def median_background(datacube, sigma):
     return np.nanmedian(datacube, 0)
 
 def sextractor_background(datacube, sigma):
-    # reading documentation TODO
-    return None
+    return stats.sigmaclip(datacube, high=sigma)
