6ed03a3187888484254604f45edce33d971a3183
EmAstro <emanuele.paolo.farina@gmail.com>
2022-07-14 19:54:25 -1000

    DataContainers

--- pycube/datacontainers/datacontainer.py
+++ pycube/datacontainers/datacontainer.py
@@ -1,5 +1,6 @@
 from astropy.io import fits
 from pycube.ancillary import checks
+from pycube import msgs
 
 __all__ = ['DataContainer']
 
@@ -37,11 +38,16 @@ class DataContainer:
 
     @fits_file.setter
     def fits_file(self, fits_file):
-        if checks.fits_file_is_valid(fits_file):
+        if fits_file is None:
+            self._fits_file = None
+        elif checks.fits_file_is_valid(fits_file):
             self._fits_file = fits_file
+            msgs.work('Loading datacube...')
+            self.hdul = fits.open(fits_file)
+            msgs.info('Datacube loaded')
         else:
             raise ValueError('Error in reading in {}'.format(fits_file))
-        self.hdul = fits.open(fits_file)
+
 
 
 
--- pycube/instruments/vlt_muse.py
+++ pycube/instruments/vlt_muse.py
@@ -3,9 +3,9 @@ from pycube.instruments import instrument
 
 class VLTMuse(instrument.Instrument):
     def __init__(self):
-        self.primary_extension='PRIMARY'
-        self.data_extension='DATA'
-        self.sigma_extension='STAT'
+        self.primary_extension = 'PRIMARY'
+        self.data_extension = 'DATA'
+        self.sigma_extension = 'STAT'
         self.telescope = 'VLT'  # ToDo this will be made a class
         self.name = 'MUSE'
 
