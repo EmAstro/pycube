d9092656a6504f612b6ff5e646522107a5631f8f
EmAstro <emanuele.paolo.farina@gmail.com>
2022-08-10 17:04:41 -1000

    updating datacontainer

--- pycube/fake_sources/fake_source.py
+++ pycube/fake_sources/fake_source.py
@@ -1,10 +1,19 @@
+from astropy.modeling import models
+
 
 class FakeSource:
     """"
 
     """
-    def __init__(self):
-        self.location = None, None, None
-        self.spatial_shape = None
-        self.spectral_shape = None
+    def __init__(self, location=(None, None, None), spatial_shape=None, spectral_shape=None):
+        self.location = location
+        self.spatial_shape = spatial_shape
+        self.spectral_shape = spectral_shape
+
+
+class GaussianSource(FakeSource):
+    r"""This class is designed
+    """
+    def __init__(self, location=None, spectral_shape=None):
+        super().__init__(location=location, spatial_shape=models.Gaussian2D(), spectral_shape=spectral_shape)
 
