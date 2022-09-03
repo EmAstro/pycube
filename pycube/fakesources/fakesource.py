c315c21e409b6671bcecd749241d292512e32ab8
EmAstro <emanuele.paolo.farina@gmail.com>
2022-09-02 16:17:43 -1000

    updating source creation

--- pycube/fakesources/fakesource.py
+++ pycube/fakesources/fakesource.py
@@ -5,22 +5,28 @@ class FakeSource:
     """"
 
     """
-    def __init__(self, x_spatial=None, y_spatial=None, z_spectral=None, spatial_shape=None, spectral_shape=None):
+    def __init__(self, x_spatial=None, y_spatial=None, z_spectral=None,
+                 spatial_shape=None, spectral_shape=None):
         self.x_spatial = x_spatial
         self.y_spatial = y_spatial
         self.z_spectral = z_spectral
         self.spatial_shape = spatial_shape
         self.spectral_shape = spectral_shape
 
+        def evaluate():
+            return
+
 
 class GaussianSource(FakeSource):
     r"""This class is designed
     """
-    def __init__(self, x_spatial=None, y_spatial=None, z_spectral=None, spectral_shape=None,
-                 **kwargs):
+    def __init__(self, x_spatial=None, y_spatial=None, z_spectral=None,
+                 sigma_spatial=None, sigma_spectral=None):
         super().__init__(x_spatial=x_spatial, y_spatial=y_spatial, z_spectral=z_spectral,
                          spatial_shape=models.Gaussian2D(x_mean=x_spatial, y_mean=y_spatial,
-                                                         **kwargs),
-                         spectral_shape=models.Gaussian1D(mean=z_spectral))
+                                                         x_stddev=sigma_spatial, y_stddev=sigma_spatial,
+                                                         amplitude=1., theta=0.),
+                         spectral_shape=models.Gaussian1D(mean=z_spectral, stddev=sigma_spectral,
+                                                          amplitude=1.))
 
 
